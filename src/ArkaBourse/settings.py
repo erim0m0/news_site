04918a05fe56ffea45a05660f711b99daf391f9d
erim0m0 <amirj070@gmail.com>
2022-08-31 14:19:44 +0430

    first commit

--- src/ArkaBourse/settings.py
+++ src/ArkaBourse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$t05v*suxsfo3vh@8#*on3*opctphy=!5--fek)-)o5zcw))j&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
